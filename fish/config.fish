628b0b37d8566902f8283bb336d68474d78714bb
Michael van Straten <michael@vanstraten.de>
2023-04-13 19:59:08 +0200

    Removed fish nvim abbr and fixed some docs

--- fish/config.fish
+++ fish/config.fish
@@ -1,23 +1,19 @@
 # Disable welcome message
-
 set fish_greeting
 
-# Convinience abbriviations
-
+# Convenience abbreviations
 abbr -a c "clear"
-abbr -a vim "nvim"
 
 # Replacing "exa" with "ls"
-
 if command -v exa > /dev/null
 	abbr -a l "exa"
 	abbr -a ls "exa"
-	abbr -a la 'exa -a'
-	abbr -a ll 'exa -l'
-	abbr -a lla 'exa -la'
+	abbr -a la "exa -a"
+	abbr -a ll "exa -l"
+	abbr -a lla "exa -la"
 else
-	abbr -a l 'ls'
-	abbr -a la 'ls -a'
-	abbr -a ll 'ls -l'
-	abbr -a lla 'ls -la'
+	abbr -a l "ls"
+	abbr -a la "ls -a"
+	abbr -a ll "ls -l"
+	abbr -a lla "ls -la"
 end
