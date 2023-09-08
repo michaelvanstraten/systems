84bbd663645aff697948c44014d0a8a86c95eacd
Michael van Straten <michael@vanstraten.de>
2023-09-08 16:29:43 +0200

    Improved bootstrap script: added logfile, better error handling

--- .scripts/bootstrap.sh
+++ .scripts/bootstrap.sh
@@ -6,11 +6,11 @@ YELLOW="\033[1;33m"
 RED="\033[0;31m"
 RESET="\033[0m"
 
-# Define the GitHub repository for your dotfiles
+# Configuration options
 DOTFILES_REPO="https://github.com/michaelvanstraten/dotfiles.git"
-
-# Directory for storing backups
+BREWFILE="$HOME/.homebrew/Brewfile"
 BACKUP_DIR="$HOME/.dotfiles-backup"
+LOG_FILE="$HOME/.dotfiles-install.log"
 
 # Function to print colored messages
 print_message() {
@@ -19,28 +19,51 @@ print_message() {
     echo -e "${color}${message}${RESET}"
 }
 
-# Check if the script is running on macOS
-if [[ "$(uname -s)" != "Darwin" ]]; then
-    print_message "${YELLOW}" "Skipping installation of Git because not on macOS."
-else
-    # Check if Homebrew is installed
-    if ! command -v brew &> /dev/null; then
-        print_message "${YELLOW}" "Installing Homebrew..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-    fi
+# Function to check if a command is available
+command_exists() {
+    command -v "$1" &> /dev/null
+}
 
-    # Check if Git is installed
-    if ! command -v git &> /dev/null; then
-        print_message "${YELLOW}" "Installing Git..."
+# Check for dependencies
+if ! command_exists "curl" || ! command_exists "awk"; then
+    print_message "${RED}" "Error: Required dependencies (curl and awk) not found. Please install them manually and rerun the script."
+    exit 1
+fi
+
+# Function to install Homebrew on macOS
+install_homebrew() {
+    print_message "${YELLOW}" "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+# Function to install Git if not already installed
+install_git() {
+    print_message "${YELLOW}" "Installing Git..."
+    if [[ "$(uname -s)" == "Darwin" ]]; then
         brew install git
     else
-        print_message "${YELLOW}" "Git is already installed."
+        print_message "${RED}" "Error: Git is not installed. Please install Git manually and rerun the script."
+        exit 1
+    fi
+}
+
+# Check if Homebrew is installed (macOS only)
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    if ! command_exists "brew"; then
+        install_homebrew
+        eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
 fi
 
+# Check if Git is installed
+if ! command_exists "git"; then
+    install_git
+else
+    print_message "${YELLOW}" "Git is already installed."
+fi
+
 # Clone the dotfiles repository
-git clone --bare "$DOTFILES_REPO" "$HOME/.dotfiles"
+git clone --bare "$DOTFILES_REPO" "$HOME/.dotfiles" &>> "$LOG_FILE"
 
 # Define a function to work with dotfiles
 function dotfiles {
@@ -62,11 +85,12 @@ done
 # Set status.showUntrackedFiles to no to ignore untracked files
 dotfiles config status.showUntrackedFiles no
 
-# Install Packages using Brewfile
-BREWFILE="$HOME/.homebrew/Brewfile"
-if [ -f "$BREWFILE" ]; then
+# Install Packages using Brewfile (macOS only)
+if [[ "$(uname -s)" == "Darwin" ]] && [ -f "$BREWFILE" ]; then
     print_message "${YELLOW}" "Installing packages from Brewfile..."
-    brew bundle --file "$BREWFILE"
-else
+    brew bundle --file "$BREWFILE" &>> "$LOG_FILE"
+elif [[ "$(uname -s)" == "Darwin" ]]; then
     print_message "${YELLOW}" "Brewfile not found. No packages to install."
 fi
+
+print_message "${GREEN}" "Dotfiles installation completed. Check $LOG_FILE for details."
