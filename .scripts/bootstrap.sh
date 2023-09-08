d5810323b67cc2b17c11281348cadae561e8c80d
Michael van Straten <michael@vanstraten.de>
2023-09-08 16:00:40 +0200

    eval homebrew shellenv script after install

--- .scripts/bootstrap.sh
+++ .scripts/bootstrap.sh
@@ -27,6 +27,7 @@ else
     if ! command -v brew &> /dev/null; then
         print_message "${YELLOW}" "Installing Homebrew..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
 
     # Check if Git is installed
