a3f249b85835a5058f676f3b76a804dcba35581f
Michael van Straten <michael@vanstraten.de>
2023-10-29 12:21:35 +0100

    Toggle startinsert when opening lazygit

--- .config/nvim/lua/custom/plugins/configs/lazygit.lua
+++ .config/nvim/lua/custom/plugins/configs/lazygit.lua
@@ -9,6 +9,15 @@ local lazygit = Terminal:new {
     float_opts = {
         border = "single",
     },
+    -- function to run on opening the terminal
+    on_open = function(term)
+        vim.cmd "startinsert!"
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+    end,
+    -- function to run on closing the terminal
+    on_close = function()
+        vim.cmd "startinsert!"
+    end,
 }
 
 M.toggle = function()
