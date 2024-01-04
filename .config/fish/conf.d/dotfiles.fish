d70c06f17bdb900a1bce6eed447a4d764d264046
Michael van Straten <michael@vanstraten.de>
2024-01-04 19:18:43 +0100

    Formated files using fish_indent

--- .config/fish/conf.d/dotfiles.fish
+++ .config/fish/conf.d/dotfiles.fish
@@ -5,20 +5,19 @@ function dotfiles --wraps git
             switch (string lower $argv[2])
                 case nvchad
                     set prefix ".config/nvim/"
-                    set repository "NvChad"
+                    set repository NvChad
                     set ref "v3.0"
                 case betterfox
                     set prefix "Library/Application Support/Firefox/Profiles/rf4bf0gc.michael/"
-                    set repository "Betterfox"
-                    set ref "main"
+                    set repository Betterfox
+                    set ref main
                 case "*"
                     echo "Unknown dotfiles subtree."
                     return 1
             end
-            
+
             cd $HOME && dotfiles subtree pull --prefix $prefix $repository $ref --squash
         case "*"
             git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
     end
 end
-
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,19 @@
 # Disable welcome message
 set fish_greeting
 
-set -Ux EDITOR nvim                     # Use neovim as default editor
+set -Ux EDITOR nvim # Use neovim as default editor
 set -Ux XDG_CONFIG_HOME "$HOME/.config" # Set to common Linux tools recognise config paths
 
 # Convenience abbreviations
-abbr -a c "clear" 
-abbr -a vim "nvim" # vim to nvim
-abbr -a npm "pnpm" # npm to pnpm
+abbr -a c clear
+abbr -a vim nvim # vim to nvim
+abbr -a npm pnpm # npm to pnpm
 abbr -a tn "tmux new -s (pwd | sed 's/.*\///g')" # new tmux session with the name of the current dir
 
 
 # Setup zoxide
 if command -q zoxide
-    zoxide init fish | source 
+    zoxide init fish | source
 end
 
 # Setup smart tmux session manager
@@ -21,16 +21,16 @@ fish_add_path $HOME/.tmux/plugins/t-smart-tmux-session-manager/bin
 
 # Replacing "ls" with "eza"
 if command -q eza
-	abbr -a l "eza"
-	abbr -a ls "eza"
-	abbr -a la "eza -a"
-	abbr -a ll "eza -l"
-	abbr -a lla "eza -la"
+    abbr -a l eza
+    abbr -a ls eza
+    abbr -a la "eza -a"
+    abbr -a ll "eza -l"
+    abbr -a lla "eza -la"
 else
-	abbr -a l "ls"
-	abbr -a la "ls -a"
-	abbr -a ll "ls -l"
-	abbr -a lla "ls -la"
+    abbr -a l ls
+    abbr -a la "ls -a"
+    abbr -a ll "ls -l"
+    abbr -a lla "ls -la"
 end
 
 # pyenv init
