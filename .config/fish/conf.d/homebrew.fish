79da6ca41634d8c95cf093878799901097e61719
Michael van Straten <michael@vanstraten.de>
2024-01-06 19:03:35 +0100

    Made homebrew fish config cross platform

--- .config/fish/conf.d/homebrew.fish
+++ .config/fish/conf.d/homebrew.fish
@@ -1,2 +1,15 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
+switch (uname -s)
+    case 'Darwin'
+        set brew_path "/opt/homebrew/bin/brew"
+    case 'Linux'
+        set brew_path "/home/linuxbrew/.linuxbrew/bin/brew"
+    case '*'
+        echo "Unsupported operating system"
+        return
+end
+
+# Set Homebrew environment
+eval "$($brew_path shellenv)"
+
+# Disable auto-update
 set -gx HOMEBREW_NO_AUTO_UPDATE 1
