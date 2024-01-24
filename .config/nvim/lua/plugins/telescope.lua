7dd86a0d6474b2a574920d2a8a79df736ae272c4
Michael van Straten <michael@vanstraten.de>
2024-01-24 17:54:39 +0100

    Added new keymap for telescope to show keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,8 @@
 return {
     "nvim-telescope/telescope.nvim",
     keys = {
+        -- press "?" to show key maps
+        { "?", "<cmd>Telescope keymaps<cr>", desc = "Show keymaps" },
         -- "<leader>ss" to spell suggest word
         { "<leader>ss", "<cmd>Telescope spell_suggest<cr>", desc = "Spell suggest" },
     },
