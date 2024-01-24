4313d1eceff1f8175a04960764e4807cca4f6291
Michael van Straten <michael@vanstraten.de>
2024-01-24 17:30:39 +0100

    Set colorscheme to catppuccin mocha

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        opts = {
+            flavour = "mocha", -- latte, frappe, macchiato, mocha
+            transparent_background = true,
+        },
+    },
+
+    -- Configure LazyVim to load catppuccin
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin",
+        },
+    },
+}
