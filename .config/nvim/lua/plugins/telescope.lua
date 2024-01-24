4cf6d644f67f123c54a19eb6af4da054af33b5f8
Michael van Straten <michael@vanstraten.de>
2024-01-24 17:54:18 +0100

    Readded spell suggest keymap for telescope

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,7 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+        -- "<leader>ss" to spell suggest word
+        { "<leader>ss", "<cmd>Telescope spell_suggest<cr>", desc = "Spell suggest" },
+    },
+}
