1e23edb68fa914493cc0d21f9794a95a80cf2537
Michael van Straten <michael@vanstraten.de>
2024-01-27 00:25:21 +0100

    Formated file

--- .scripts/bootstrap.sh
+++ .scripts/bootstrap.sh
@@ -14,123 +14,123 @@ RESET="\033[0m"
 
 # Function to print colored messages
 print_message() {
-    color="$1"
-    message="$2"
-    printf "%b%b%b\n" "$color" "$message" "$RESET"
+	color="$1"
+	message="$2"
+	printf "%b%b%b\n" "$color" "$message" "$RESET"
 }
 
 # Function to print an error message and exit
 exit_with_error() {
-    print_message "$RED" "Error: $1"
-    exit 1
+	print_message "$RED" "Error: $1"
+	exit 1
 }
 
 # Function to check if a command is available
 command_exists() {
-    command -v "$1" >/dev/null 2>&1
+	command -v "$1" >/dev/null 2>&1
 }
 
 get() {
-    if command_exists "curl";  then
-        curl -fsSL "$1"
-    elif command_exists "wget"; then
-        wget -qO- "$1"
-    else
-        exit_with_error "Please install wget or curl"
-    fi
+	if command_exists "curl"; then
+		curl -fsSL "$1"
+	elif command_exists "wget"; then
+		wget -qO- "$1"
+	else
+		exit_with_error "Please install wget or curl"
+	fi
 }
 
 # Define a function to work with dotfiles
 dotfiles() {
-    git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" "$@"
+	git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" "$@"
 }
 
 # Function to confirm actions interactively
 confirm_action() {
-    prompt="$1"
-    printf "%s (y/N): " "$prompt"
-    read -r response
-    if [ "$response" = "y" ] || [ "$response" = "Y" ]; then
-        return 0
-    else
-        return 1
-    fi
+	prompt="$1"
+	printf "%s (y/N): " "$prompt"
+	read -r response
+	if [ "$response" = "y" ] || [ "$response" = "Y" ]; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 # Function to install Homebrew if not installed
 install_homebrew() {
-    if ! command_exists "brew"; then
-        print_message "$YELLOW" "Installing Homebrew..."
-        sh -c "$(get https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	if ! command_exists "brew"; then
+		print_message "$YELLOW" "Installing Homebrew..."
+		sh -c "$(get https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-        case "$(uname -s)" in
-            Darwin)
-                brew_path="/opt/homebrew/bin/brew"
-                ;;
-            Linux)
-                if [ -d "$HOME/.linuxbrew" ]; then
-                    brew_path="$HOME/.linuxbrew/bin/brew"
-                elif [ -d "/home/linuxbrew/.linuxbrew" ]; then
-                    brew_path="/home/linuxbrew/.linuxbrew/bin/brew"
-                else
-                    exit_with_error "Linuxbrew not found in expected locations."
-                fi
-                ;;
-            *)
-                exit_with_error "Unsupported operating system"
-                ;;
-        esac
+		case "$(uname -s)" in
+		Darwin)
+			brew_path="/opt/homebrew/bin/brew"
+			;;
+		Linux)
+			if [ -d "$HOME/.linuxbrew" ]; then
+				brew_path="$HOME/.linuxbrew/bin/brew"
+			elif [ -d "/home/linuxbrew/.linuxbrew" ]; then
+				brew_path="/home/linuxbrew/.linuxbrew/bin/brew"
+			else
+				exit_with_error "Linuxbrew not found in expected locations."
+			fi
+			;;
+		*)
+			exit_with_error "Unsupported operating system"
+			;;
+		esac
 
-        # Set Homebrew environment
-        eval "$("$brew_path" shellenv)"v
-    else
-        print_message "$GREEN" "Homebrew is already installed."
-    fi
+		# Set Homebrew environment
+		eval "$("$brew_path" shellenv)"v
+	else
+		print_message "$GREEN" "Homebrew is already installed."
+	fi
 }
 
 # Function to handle dotfiles installation
 install_dotfiles() {
-    # Clone dotfiles repository
-    if [ -d "$HOME/.dotfiles" ]; then
-        print_message "$GREEN" "Dotfiles repository already exists. Skipping clone."
-    else
-        print_message "$YELLOW" "Cloning dotfiles repository."
-        git clone --bare "$DOTFILES_REPO" "$HOME/.dotfiles" >> "$LOG_FILE" 2>&1
-    fi
+	# Clone dotfiles repository
+	if [ -d "$HOME/.dotfiles" ]; then
+		print_message "$GREEN" "Dotfiles repository already exists. Skipping clone."
+	else
+		print_message "$YELLOW" "Cloning dotfiles repository."
+		git clone --bare "$DOTFILES_REPO" "$HOME/.dotfiles" >>"$LOG_FILE" 2>&1
+	fi
 
-    # Handle existing dotfiles backup
-    if [ -d "$BACKUP_DIR" ]; then
-        confirm_action "There already exists a backup of your dotfiles, proceeding would delete that backup." || return
-        rm -r "$BACKUP_DIR"
-    fi
+	# Handle existing dotfiles backup
+	if [ -d "$BACKUP_DIR" ]; then
+		confirm_action "There already exists a backup of your dotfiles, proceeding would delete that backup." || return
+		rm -r "$BACKUP_DIR"
+	fi
 
-    # Attempt to checkout dotfiles
-    if [ -d "$HOME/.dotfiles" ]; then
-        mkdir -p "$BACKUP_DIR"
-        dotfiles checkout 2>&1 | while read -r file; do
-            if echo "$file" | grep -q "already exists"; then
-                print_message "$RED" "Backing up pre-existing dotfiles."
-                dotfiles checkout 2>&1 | awk '/\s+\./ {print $1}' | xargs -I{} mv {} "$BACKUP_DIR/{}"
-                break
-            fi
-        done
-    fi
+	# Attempt to checkout dotfiles
+	if [ -d "$HOME/.dotfiles" ]; then
+		mkdir -p "$BACKUP_DIR"
+		dotfiles checkout 2>&1 | while read -r file; do
+			if echo "$file" | grep -q "already exists"; then
+				print_message "$RED" "Backing up pre-existing dotfiles."
+				dotfiles checkout 2>&1 | awk '/\s+\./ {print $1}' | xargs -I{} mv {} "$BACKUP_DIR/{}"
+				break
+			fi
+		done
+	fi
 
-    # Set status.showUntrackedFiles to no to ignore untracked files
-    dotfiles config status.showUntrackedFiles no
+	# Set status.showUntrackedFiles to no to ignore untracked files
+	dotfiles config status.showUntrackedFiles no
 
-    # Hide readme.md if on macOS
-    if [ "$(uname -s)" = "Darwin" ]; then
-        chflags hidden "$HOME/readme.md"
-    fi
+	# Hide readme.md if on macOS
+	if [ "$(uname -s)" = "Darwin" ]; then
+		chflags hidden "$HOME/readme.md"
+	fi
 
-    # Add remote sub-tree repositories
-    if ! dotfiles remote | grep -q "NvChad"; then
-        dotfiles remote add NvChad https://github.com/NvChad/NvChad
-    fi
-    if ! dotfiles remote | grep -q "Betterfox"; then
-        dotfiles remote add Betterfox https://github.com/yokoffing/Betterfox
-    fi
+	# Add remote sub-tree repositories
+	if ! dotfiles remote | grep -q "NvChad"; then
+		dotfiles remote add NvChad https://github.com/NvChad/NvChad
+	fi
+	if ! dotfiles remote | grep -q "Betterfox"; then
+		dotfiles remote add Betterfox https://github.com/yokoffing/Betterfox
+	fi
 }
 
 # Check if OS is compatible.
@@ -144,28 +144,28 @@ install_dotfiles
 
 # Install Packages using Brewfile
 if [ -f "$BREWFILE" ] && confirm_action "Install homebrew packages from Brewfile"; then
-    EDITOR="${EDITOR:-vi}"
-    confirm_action "Edit Brewfile before installing" && $EDITOR "$BREWFILE"
-    print_message "$GREEN" "Installing packages from Brewfile..."
-    brew bundle --file "$BREWFILE" 2>&1 | tee "$LOG_FILE"
+	EDITOR="${EDITOR:-vi}"
+	confirm_action "Edit Brewfile before installing" && $EDITOR "$BREWFILE"
+	print_message "$GREEN" "Installing packages from Brewfile..."
+	brew bundle --file "$BREWFILE" 2>&1 | tee "$LOG_FILE"
 else
-    print_message "$YELLOW" "No packages to install."
+	print_message "$YELLOW" "No packages to install."
 fi
 
 # Install tmux plugin manager (tpm) if tmux is installed
 if command_exists "tmux"; then
-    print_message "$YELLOW" "Installing tmux plugin manager."
-    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+	print_message "$YELLOW" "Installing tmux plugin manager."
+	git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
 fi
 
 os_config="$HOME/.scripts/sys/$(uname | tr '[:upper:]' '[:lower:]').sh"
 if [ -f "$os_config" ] && confirm_action "Set os specific configuration"; then
-    . "$os_config"
+	. "$os_config"
 fi
 
 print_message "$GREEN" "Dotfiles installation completed. Check $LOG_FILE for details."
 
 # Log user out for settings to take effect
 if [ "$(uname -s)" = "Darwin" ] && confirm_action "You may want to logout and log back in for many settings to take effect."; then
-    osascript -e 'tell app "System Events" to log out'
+	osascript -e 'tell app "System Events" to log out'
 fi
