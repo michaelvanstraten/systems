eda64a0f3ac0352c1357fe25512854028777224c
Michael van Straten <michael@vanstraten.de>
2024-05-28 00:37:33 +0200

    Setup proper keymap and client capabilities for lsp clients

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,3 +1,39 @@
+local function on_client_attach(_, buffer_number)
+	local map = vim.keymap.set
+
+	map(
+		"n",
+		"ca",
+		vim.lsp.buf.code_action,
+		{ buffer = buffer_number, desc = "Selects a code action available at the current cursor position" }
+	)
+	map(
+		"n",
+		"cn",
+		vim.lsp.buf.rename,
+		{ buffer = buffer_number, desc = "Renames all references to the symbol under the cursor" }
+	)
+end
+
+local function make_client_options(opts)
+	opts = opts or {}
+
+	local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+	local client_capabilities = vim.tbl_deep_extend(
+		"force",
+		{},
+		vim.lsp.protocol.make_client_capabilities(),
+		cmp_nvim_lsp.default_capabilities() or {},
+		opts.capabilities or {}
+	)
+
+	return vim.tbl_deep_extend("force", {
+		capabilities = client_capabilities,
+		on_attach = on_client_attach,
+	}, opts or {})
+end
+
 return {
 	"neovim/nvim-lspconfig",
 	event = "VeryLazy",
@@ -8,17 +44,8 @@ return {
 	},
 	config = function()
 		local lspconfig = require("lspconfig")
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(),
-			cmp_nvim_lsp.default_capabilities()
-		)
-
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
+		lspconfig.lua_ls.setup(make_client_options({
 			settings = {
 				Lua = {
 					workspace = {
@@ -32,15 +59,13 @@ return {
 					},
 				},
 			},
-		})
-		lspconfig.texlab.setup({ capabilities = capabilities })
-		lspconfig.rust_analyzer.setup({
-			capabilities = capabilities,
-		})
-		lspconfig.nil_ls.setup({ capabilities = capabilities })
-		lspconfig.yamlls.setup({ capabilities = capabilities })
-		lspconfig.pyright.setup({ capabilities = capabilities })
-		lspconfig.clangd.setup({ capabilities = capabilities })
-		lspconfig.ltex.setup({ capabilities = capabilities })
+		}))
+		lspconfig.texlab.setup(make_client_options())
+		lspconfig.rust_analyzer.setup(make_client_options())
+		lspconfig.nil_ls.setup(make_client_options())
+		lspconfig.yamlls.setup(make_client_options())
+		lspconfig.pyright.setup(make_client_options())
+		lspconfig.clangd.setup(make_client_options())
+		lspconfig.ltex.setup(make_client_options())
 	end,
 }
