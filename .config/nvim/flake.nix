ed1b2098967b61cf43b3372cf2d7261832f95fe4
Michael van Straten <michael@vanstraten.de>
2024-08-10 15:11:51 +0200

    Add subflake

---
+++ .config/nvim/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    lazy2nix.url = "/Users/michaelvanstraten/Projects/lazy2nix/";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      lazy2nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default =
+        ((lazy2nix.lib { pkgs = import nixpkgs { inherit system; }; }).mkNeovimConfiguration { configDir = ./.; })
+        .neovim;
+    });
+}
