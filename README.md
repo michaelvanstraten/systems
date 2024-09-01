ed607a071a440c827e511db881f60ac188ec950b
Michael van Straten <michael@vanstraten.de>
2024-09-01 21:13:06 +0200

    Add flake skeleton

---
+++ README.md
@@ -0,0 +1,35 @@
+# My Systems Repository
+
+Welcome to my **Systems** repository, where I manage both my dotfiles and system
+configurations using Nix Flakes. This repository represents an evolved version
+of my previous
+[dotfiles repository](https://github.com/michaelvanstraten/dotfiles), now
+focusing on a more integrated and reproducible approach to system management.
+
+## Overview
+
+This repository leverages Nix Flakes to provide a declarative and reproducible
+setup for both my personal and professional environments. It includes
+configurations for various systems, including macOS and NixOS, ensuring a
+consistent experience across different machines.
+
+## Features
+
+- **Nix Flakes**: Utilizes Nix Flakes for declarative and reproducible system
+  configurations.
+- **Cross-Platform**: Supports both macOS and NixOS systems.
+- **Integrated Management**: Combines dotfiles and system configurations into a
+  single repository for easier management.
+- **Automated Checks**: Includes pre-commit hooks for maintaining code quality
+  and consistency.
+
+## Contributing
+
+Contributions are welcome! If you have any improvements or bug fixes, feel free
+to submit a pull request. For major changes, please open an issue first to
+discuss what you would like to change.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file
+for details.
---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nix-darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixinate = {
+      url = "github:matthewcroughan/nixinate";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    cyberdream-theme = {
+      url = "github:scottmckendry/cyberdream.nvim";
+      flake = false;
+    };
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs-firefox-darwin = {
+      url = "github:bandithedoge/nixpkgs-firefox-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+  };
+
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      nixinate,
+      nix-darwin,
+      home-manager,
+      self,
+      ...
+    }@inputs:
+    {
+      # apps = nixinate.nixinate.aarch64-linux self;
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        checks = {
+          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixfmt = {
+                enable = true;
+                package = pkgs.nixfmt-rfc-style;
+              };
+              nil.enable = true;
+              prettier = {
+                enable = true;
+                settings = {
+                  prose-wrap = "always";
+                };
+              };
+            };
+          };
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            inherit (checks.pre-commit-check) shellHook;
+            buildInputs = checks.pre-commit-check.enabledPackages;
+          };
+        };
+      }
+    );
+}
