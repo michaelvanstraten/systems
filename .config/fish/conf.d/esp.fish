3d7dec79b0b4b6605022f3ef2f1a2a0b8cba85aa
Michael van Straten <michael@vanstraten.de>
2024-09-27 18:29:06 +0200

    Remove unused esp config

--- .config/fish/conf.d/esp.fish
+++
@@ -1,5 +0,0 @@
-# set -x LIBCLANG_PATH "/Users/michaelvanstraten/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-16.0.0-20230516/esp-clang/lib"
-# set -x PATH "/Users/michaelvanstraten/.rustup/toolchains/esp/xtensa-esp32s2-elf/esp-12.2.0_20230208/xtensa-esp32s2-elf/bin" $PATH
-# set -x PATH "/Users/michaelvanstraten/.rustup/toolchains/esp/xtensa-esp32s3-elf/esp-12.2.0_20230208/xtensa-esp32s3-elf/bin" $PATH
-# set -x PATH "/Users/michaelvanstraten/.rustup/toolchains/esp/xtensa-esp32-elf/esp-12.2.0_20230208/xtensa-esp32-elf/bin" $PATH
-# set -x PATH "/Users/michaelvanstraten/.rustup/toolchains/esp/riscv32-esp-elf/esp-12.2.0_20230208/riscv32-esp-elf/bin" $PATH
