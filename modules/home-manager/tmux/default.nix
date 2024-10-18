2f34291f574a9036c04a3c46ccbb328633907a3d
Michael van Straten <michael@vanstraten.de>
2024-10-18 16:39:25 +0200

    fix: tmux no longer uses the correct shell
    See https://github.com/nix-community/home-manager/issues/5952

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -5,6 +5,8 @@
 
     extraConfig = builtins.readFile ./tmux.conf;
 
+    sensibleOnTop = false;
+
     plugins = with pkgs.tmuxPlugins; [
       # Enables seamless navigation between tmux panes and vim splits
       vim-tmux-navigator
