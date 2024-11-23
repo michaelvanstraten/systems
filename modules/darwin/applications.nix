66e1d945389671a98ade3cc6094913884bd272fa
Michael van Straten <mvanstraten@mozilla.com>
2024-11-23 12:23:30 +0100

    Remove old, nix-darwin applications symlink if it exists

--- modules/darwin/applications.nix
+++ modules/darwin/applications.nix
@@ -18,6 +18,16 @@
 
         echo "setting up $global_nix_apps..."
 
+        ourLink () {
+          local link
+          link=$(readlink "$1")
+          [ -L "$1" ] && [ "''${link#*-}" = 'system-applications/Applications' ]
+        }
+
+        if ourLink "$global_nix_apps"; then
+          rm "$global_nix_apps"
+        fi
+
         ${mac-app-util.packages.${pkgs.system}.default}/bin/mac-app-util \
           sync-trampolines "${config.system.build.applications}/Applications" \
           "$global_nix_apps"
