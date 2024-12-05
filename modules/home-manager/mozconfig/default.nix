11c142cfe65f4412fc0ffab514d152b44397da88
Michael van Straten <mvanstraten@mozilla.com>
2024-12-05 17:40:38 +0100

    Set `MOZCONFIG` env variable

--- modules/home-manager/mozconfig/default.nix
+++ modules/home-manager/mozconfig/default.nix
@@ -2,4 +2,8 @@
   home.file.".mozconfig/mozconfig-ff-dbg".source = ./mozconfig-ff-dbg;
   home.file.".mozconfig/mozconfig-ff-rel-dbg".source = ./mozconfig-ff-rel-dbg;
   home.file.".mozconfig/mozconfig-ff-rel-opt".source = ./mozconfig-ff-rel-opt;
+
+  home.sessionVariables = {
+    MOZCONFIG = ./mozconfig-ff-dbg;
+  };
 }
