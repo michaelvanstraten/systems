0bff1d741dabe3f71104e71127ccfb5b39d8a605
Michael van Straten <michael@vanstraten.de>
2025-04-16 17:54:33 +0200

    Switch from nvim-cmp to blink and improve lspconfig setup

--- modules/home-manager/nvim/lua/plugins/lsp/lspconfig.lua
+++ modules/home-manager/nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,36 +1,9 @@
-local function on_client_attach(_, buffer_number)
-    local map = vim.keymap.set
-
-    map("n", "ca", vim.lsp.buf.code_action, {
-        buffer = buffer_number,
-        desc = "Selects a code action available at the current cursor position",
-    })
-    map(
-        "n",
-        "cn",
-        vim.lsp.buf.rename,
-        { buffer = buffer_number, desc = "Renames all references to the symbol under the cursor" }
-    )
-end
-
-local function make_client_options(opts)
-    opts = opts or {}
-
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-    local client_capabilities = vim.tbl_deep_extend(
-        "force",
-        {},
-        vim.lsp.protocol.make_client_capabilities(),
-        cmp_nvim_lsp.default_capabilities() or {},
-        opts.capabilities or {}
-    )
-
-    return vim.tbl_deep_extend("force", {
-        capabilities = client_capabilities,
-        on_attach = on_client_attach,
-    }, opts or {})
-end
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = "rounded",
+    anchor_bias = "above",
+    max_height = math.floor(vim.o.lines * 0.5),
+    max_width = math.floor(vim.o.columns * 0.75),
+})
 
 return {
     "neovim/nvim-lspconfig",
@@ -38,32 +11,54 @@ return {
     dependencies = {
         { "folke/neoconf.nvim", cmd = "Neoconf", opts = {}, dependencies = { "nvim-lspconfig" } },
         { "folke/neodev.nvim", opts = {} },
-        "hrsh7th/cmp-nvim-lsp",
+        "saghen/blink.cmp",
     },
-    config = function()
-        local lspconfig = require("lspconfig")
-
-        lspconfig.lua_ls.setup(make_client_options({
-            settings = {
-                Lua = {
-                    workspace = {
-                        checkThirdParty = false,
-                    },
-                    codeLens = {
-                        enable = true,
-                    },
-                    completion = {
-                        callSnippet = "Replace",
+    opts = {
+        servers = {
+            lua_ls = {
+                settings = {
+                    Lua = {
+                        workspace = {
+                            checkThirdParty = false,
+                        },
+                        codeLens = {
+                            enable = true,
+                        },
+                        completion = {
+                            callSnippet = "Replace",
+                        },
                     },
                 },
             },
-        }))
-        lspconfig.texlab.setup(make_client_options())
-        lspconfig.rust_analyzer.setup(make_client_options())
-        lspconfig.nixd.setup(make_client_options())
-        lspconfig.yamlls.setup(make_client_options())
-        lspconfig.clangd.setup(make_client_options())
-        lspconfig.pylsp.setup(make_client_options())
-        lspconfig.ltex.setup(make_client_options())
+            texlab = {},
+            rust_analyzer = {},
+            nixd = {},
+            yamlls = {},
+            clangd = {},
+            pylsp = {},
+            ltex = {},
+            atlab_ls = {},
+        },
+    },
+    config = function(_, opts)
+        local lspconfig = require("lspconfig")
+        for server, config in pairs(opts.servers) do
+            local blink = require("blink.cmp")
+
+            config.capabilities = blink.get_lsp_capabilities(config.capabilities)
+            config.on_client_attach = function(_, buffer_number)
+                local map = vim.keymap.set
+                map("n", "ca", vim.lsp.buf.code_action, {
+                    buffer = buffer_number,
+                    desc = "Selects a code action available at the current cursor position",
+                })
+                map("n", "cn", vim.lsp.buf.rename, {
+                    buffer = buffer_number,
+                    desc = "Renames all references to the symbol under the cursor",
+                })
+            end
+
+            lspconfig[server].setup(config)
+        end
     end,
 }
