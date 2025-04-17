610f6342993c9766506496f5f7071857de8be87c
Michael van Straten <mvanstraten@mozilla.com>
2025-04-17 09:26:17 +0200

    Switch back to pyright as python LS

--- modules/home-manager/nvim/lua/plugins/lsp/lspconfig.lua
+++ modules/home-manager/nvim/lua/plugins/lsp/lspconfig.lua
@@ -35,7 +35,7 @@ return {
             nixd = {},
             yamlls = {},
             clangd = {},
-            pylsp = {},
+            pyright = {},
             ltex = {},
             taplo = {},
         },
