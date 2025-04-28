234b21eaa36a7fca63dce0a0946353c0531eff5e
Michael van Straten <michael@vanstraten.de>
2025-04-28 21:49:59 +0200

    Fix mini.surround adding spacing between braces

--- modules/home-manager/nvim/lua/plugins/mini.lua
+++ modules/home-manager/nvim/lua/plugins/mini.lua
@@ -57,14 +57,23 @@ return {
         "echasnovski/mini.surround",
         event = "VeryLazy",
         opts = {
-            mappings = {
-                add = "sa",
-                delete = "sd",
-                find = "sf",
-                find_left = "sF",
-                highlight = "sh",
-                replace = "sr",
-                update_n_lines = "sn",
+            custom_surroundings = {
+                ["("] = {
+                    input = { "%b()", "^.%s*().-()%s*.$" },
+                    output = { left = "(", right = ")" },
+                },
+                ["["] = {
+                    input = { "%b[]", "^.%s*().-()%s*.$" },
+                    output = { left = "[", right = "]" },
+                },
+                ["{"] = {
+                    input = { "%b{}", "^.%s*().-()%s*.$" },
+                    output = { left = "{", right = "}" },
+                },
+                ["<"] = {
+                    input = { "%b<>", "^.%s*().-()%s*.$" },
+                    output = { left = "<", right = ">" },
+                },
             },
         },
     },
