294dcd7ba3c992828b3c3aca75c8c0cc8d07803f
Michael van Straten <michael@vanstraten.de>
2025-04-28 21:51:00 +0200

    Remove unused mini setup

--- modules/home-manager/nvim/lua/plugins/mini.lua
+++ modules/home-manager/nvim/lua/plugins/mini.lua
@@ -1,10 +1,5 @@
 return {
     {
-        "echasnovski/mini.comment",
-        event = "VeryLazy",
-        opts = {},
-    },
-    {
         "echasnovski/mini.ai",
         event = "VeryLazy",
         opts = function()
@@ -12,23 +7,6 @@ return {
 
             return {
                 custom_textobjects = {
-                    o = ai.gen_spec.treesitter({
-                        a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-                        i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-                    }, {}),
-                    f = ai.gen_spec.treesitter(
-                        { a = "@function.outer", i = "@function.inner" },
-                        {}
-                    ),
-                    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-                    g = function() -- Whole buffer, similar to `gg` and 'G' motion
-                        local from = { line = 1, col = 1 }
-                        local to = {
-                            line = vim.fn.line("$"),
-                            col = math.max(vim.fn.getline("$"):len(), 1),
-                        }
-                        return { from = from, to = to }
-                    end,
                     ["$"] = ai.gen_spec.pair("$", "$", { type = "greedy" }),
                 },
             }
