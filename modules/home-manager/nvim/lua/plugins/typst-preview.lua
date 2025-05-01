4524ce6657f0f22119f0a4804806508f4a8e87a7
Michael van Straten <michael@vanstraten.de>
2025-05-01 08:56:03 +0200

    Integrate `typst-preview` with nushell

--- modules/home-manager/nvim/lua/plugins/typst-preview.lua
+++ modules/home-manager/nvim/lua/plugins/typst-preview.lua
@@ -3,7 +3,7 @@ return {
     ft = "typst",
     version = "1.*",
     opts = {
-
+        open_cmd = "start %s",
         dependencies_bin = { ["tinymist"] = "tinymist", ["websocat"] = "websocat" },
     },
 }
