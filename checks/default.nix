c23f0c38b4966a46d2f6182e8308ce51ec4dc876
Michael van Straten <michael@vanstraten.de>
2025-06-04 19:41:23 +0200

    feat(nix flake check): Add more checks

--- checks/default.nix
+++ checks/default.nix
@@ -8,17 +8,29 @@
   pre-commit-check = pre-commit-hooks.lib.${system}.run {
     src = ./.;
     hooks = {
+      # Nix linting and formatting
       nil = {
         enable = true;
         package = nil_ls.packages.${system}.default;
       };
       nixfmt-rfc-style.enable = true;
+      # Disabled until pipe-operator support lands.
+      # See https://github.com/oppiliappan/statix/issues/88
+      statix.enable = false;
+
+      # General formatting
       prettier = {
         enable = true;
         settings = {
           prose-wrap = "always";
         };
       };
+
+      # Security checks
+      detect-private-keys.enable = true;
+
+      # Shell scripts
+      shfmt.enable = true;
     };
   };
 }
