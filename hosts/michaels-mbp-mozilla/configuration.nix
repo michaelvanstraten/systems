b767d770d54dfe9be3f64d8f911814e314de1a4e
Michael van Straten <mvanstraten@mozilla.com>
2025-07-03 19:58:40 +0200

    fix(redis-service): Create redis data directory with correct permissions

--- hosts/michaels-mbp-mozilla/configuration.nix
+++ hosts/michaels-mbp-mozilla/configuration.nix
@@ -30,17 +30,29 @@
 
   launchd.user.agents.redis = {
     command = lib.mkForce (
-      pkgs.writeScript "redis-start" ''
+      pkgs.writeScript "redis-start"
+        # bash
+        ''
           #! ${pkgs.stdenv.shell}
 
-        # See: https://github.com/LnL7/nix-darwin/issues/659#issuecomment-1813204545
-        if [ ! -d "/var/lib/redis/" ]; then
-          echo "creating Redis data directory..."
-          sudo mkdir -m 755 -p /var/lib/redis/
-        fi
+          REDIS_DIR="/var/lib/redis"
+          CURRENT_USER="$(whoami)"
 
-        ${config.services.redis.package}/bin/redis-server /etc/redis.conf
-      ''
+          # See: https://github.com/LnL7/nix-darwin/issues/659#issuecomment-1813204545
+          if [ ! -d "$REDIS_DIR" ]; then
+            echo "Creating Redis data directory..."
+            sudo mkdir -m 755 -p "$REDIS_DIR"
+            sudo chown "$CURRENT_USER" "$REDIS_DIR"
+          else
+            OWNER="$(stat -f %Su "$REDIS_DIR")"
+            if [ "$OWNER" != "$CURRENT_USER" ]; then
+              echo "Changing owner of $REDIS_DIR to $CURRENT_USER..."
+              sudo chown "$CURRENT_USER" "$REDIS_DIR"
+            fi
+          fi
+
+          ${config.services.redis.package}/bin/redis-server /etc/redis.conf
+        ''
     );
   };
 
