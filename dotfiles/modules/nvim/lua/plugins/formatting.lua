db451bd5e9f4e8dbadb5d997aae0885b0dbb3023
Michael van Straten <mvanstraten@mozilla.com>
2025-07-03 21:01:07 +0200

    feat(neovim-config): Setup default formatter options

--- dotfiles/modules/nvim/lua/plugins/formatting.lua
+++ dotfiles/modules/nvim/lua/plugins/formatting.lua
@@ -23,6 +23,11 @@ return {
         },
         opts = function()
             return {
+                default_format_opts = {
+                    timeout_ms = 3000,
+                    async = false,
+                    quiet = false,
+                },
                 formatters_by_ft = {
                     lua = { "stylua" },
                     fish = { "fish_indent" },
