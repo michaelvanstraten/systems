d7b0a0dd101ebb266c67180859762f7c610e0929
Michael van Straten <michael@vanstraten.de>
2025-08-24 17:32:16 +0200

    refactor: Pack `programs.nushell` into nested attributes

--- dotfiles/modules/nushell/default.nix
+++ dotfiles/modules/nushell/default.nix
@@ -1,13 +1,11 @@
 { lib, config, ... }:
 {
-  programs.nushell.enable = true;
-
-  programs.nushell.configFile.source = ./config.nu;
-  programs.nushell.envFile.source = ./env.nu;
-  programs.nushell.environmentVariables = builtins.mapAttrs (
-    _: value: toString value
-  ) config.home.sessionVariables;
-  programs.nushell.shellAliases = builtins.mapAttrs (
-    _: value: lib.mkForce (builtins.replaceStrings [ "$(" ] [ "(" ] value)
-  ) config.home.shellAliases;
+  programs.nushell = {
+    configFile.source = ./config.nu;
+    envFile.source = ./env.nu;
+    environmentVariables = builtins.mapAttrs (_: value: toString value) config.home.sessionVariables;
+    shellAliases = builtins.mapAttrs (
+      _: value: lib.mkForce (builtins.replaceStrings [ "$(" ] [ "(" ] value)
+    ) config.home.shellAliases;
+  };
 }
