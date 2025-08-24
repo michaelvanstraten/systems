feddfaa707588f2c7d45c956a3c3fae128634802
Michael van Straten <michael@vanstraten.de>
2025-08-24 16:22:46 +0200

    refactor: Reorder and make sure tank pool is imported in jellyfin module

--- hosts/dxp4800plus/services/jellyfin.nix
+++ hosts/dxp4800plus/services/jellyfin.nix
@@ -1,12 +1,17 @@
 { pkgs, ... }:
 {
-  systemd.services.jellyfin.environment.LIBVA_DRIVER_NAME = "iHD";
+  boot.zfs.extraPools = [ "tank" ];
 
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "iHD";
   };
 
   nixpkgs.config.allowUnfree = true;
+
+  networking.firewall = {
+    enable = true;
+    allowPing = true;
+  };
 
   # hardware.graphics = {
   #   enable = true;
@@ -18,37 +23,42 @@
   #     intel-ocl
   #   ];
   # };
-  #
-  services.jellyfin.enable = true;
-  services.jellyfin.openFirewall = true;
 
-  services.samba = {
-    enable = true;
-    settings = {
-      global = {
-        "hosts allow" = "100.64.0.0/10 127.0.0.1 localhost";
-        "hosts deny" = "0.0.0.0/0";
-        "guest account" = "nobody";
-        "map to guest" = "bad user";
-      };
-      media = {
-        path = "/tank/media";
-        browseable = true;
-        "guest ok" = true;
-        "read only" = "no";
-        "writeable" = "yes";
-        "force user" = "nobody";
-        "force group" = "nogroup";
-        "create mask" = "0664";
-        "directory mask" = "0775";
-      };
+  services = {
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
     };
-    openFirewall = true;
+
+    samba = {
+      enable = true;
+      settings = {
+        global = {
+          "hosts allow" = "100.64.0.0/10 127.0.0.1 localhost";
+          "hosts deny" = "0.0.0.0/0";
+          "guest account" = "nobody";
+          "map to guest" = "bad user";
+        };
+        media = {
+          path = "/tank/media";
+          browseable = true;
+          "guest ok" = true;
+          "read only" = "no";
+          "writeable" = "yes";
+          "force user" = "nobody";
+          "force group" = "nogroup";
+          "create mask" = "0664";
+          "directory mask" = "0775";
+        };
+      };
+      openFirewall = true;
+    };
+
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+    };
   };
 
-  services.avahi.enable = true;
-  services.avahi.nssmdns4 = true;
-
-  networking.firewall.enable = true;
-  networking.firewall.allowPing = true;
+  systemd.services.jellyfin.environment.LIBVA_DRIVER_NAME = "iHD";
 }
