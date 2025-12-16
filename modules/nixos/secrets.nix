dde4125885fd6f29f659c688a1420167baefee50
Michael van Straten <mvanstraten@mozilla.com>
2025-12-16 22:54:04 +0100

    nixos modules: Only enable the tailscale secret if tailscale is enabled

--- modules/nixos/secrets.nix
+++ modules/nixos/secrets.nix
@@ -1,11 +1,16 @@
 { sops-nix, ... }:
-_: {
+{ config, lib, ... }:
+{
   imports = [
     sops-nix.nixosModules.sops
   ];
 
-  sops = {
-    defaultSopsFile = ../../secrets.yaml;
-    secrets."tailscale/oauth_client_secret" = { };
-  };
+  config = lib.mkMerge [
+    {
+      sops.defaultSopsFile = ../../secrets.yaml;
+    }
+    (lib.mkIf config.services.tailscale.enable {
+      sops.secrets."tailscale/oauth_client_secret" = { };
+    })
+  ];
 }
