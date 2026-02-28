7109babc34ded3886bc4996efe7f964d98622c77
Michael van Straten <michael@vanstraten.de>
2026-02-28 14:52:24 +0100

    Remove macbook-pro-mozilla config

--- hosts/macbook-pro-mozilla/outputs.nix
+++
@@ -1,9 +0,0 @@
-{ self, nix-darwin, ... }:
-{
-  darwinConfigurations = {
-    "macbook-pro-mozilla" = nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules = [ (self.lib.mkModule ./configuration.nix { }) ];
-    };
-  };
-}
