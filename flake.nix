8a14227477fba39d3f6058416230703bb39fc169
Michael van Straten <michael@vanstraten.de>
2026-05-30 12:47:05 +0200

    chore: Remove unused nixos-generators

--- flake.nix
+++ flake.nix
@@ -50,11 +50,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     cyberdream-theme = {
       url = "github:scottmckendry/cyberdream.nvim";
       flake = false;
