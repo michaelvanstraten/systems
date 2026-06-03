ae1589a7cc4a6704ee126ba9b752a576c34bd991
Michael van Straten <michael@vanstraten.de>
2026-06-03 22:49:07 +0200

    chore(mac-studio): remove host

--- hosts/mac-studio/outputs.nix
+++
@@ -1,9 +0,0 @@
-{ self, nix-darwin, ... }:
-{
-  darwinConfigurations = {
-    "fxe-mac-studio-01" = nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules = [ (self.lib.mkModule ./configuration.nix { }) ];
-    };
-  };
-}
